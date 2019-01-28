d12adf9ebcd250f453028eac6ba50f53528459d0
rsexton17 <46289015+rsexton17@users.noreply.github.com>
2019-01-28 11:46:30 -0800

    Recursive algorithm that kind of works.
    Fails to recurse the way I'd like it to, working on a solution. Does solve some cases.

---
+++ GWC.cpp
@@ -0,0 +1,44 @@
+//Goldbach's weak conjecture
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+bool IsPrime(int n) {
+  if (n == 0) return false;
+  if (n == 1) return false;
+  if (n == 2) return true;
+  for (int i = 2; i < (n/2)+1; i++) {
+    if (n % i == 0) return false;
+  }
+  return true;
+}
+
+int NextPrime(int n) {
+  while (true) {
+    n++;
+    if (IsPrime(n)) return n;
+  }
+}
+
+bool GWC(int ans, int num1, int num2, int num3) {
+  printf("%d=%d+%d+%d", ans, num1, num2, num3);
+  cout << endl;
+  if (ans == num1 + num2 + num3) {
+    printf("%d=%d+%d+%d", ans, num1, num2, num3);
+    return 1;
+  }
+  if (ans < num1 + num2 + num3) return 0;
+  else if (ans > num1 + num2 + num3) GWC(ans, num1, num2, NextPrime(num3));
+  else if (ans > num1 + num2 + num3) GWC(ans, num1, NextPrime(num2), num3);
+  else if (ans > num1 + num2 + num3) GWC(ans, NextPrime(num1), num2, num3);
+  else return 0;
+}
+
+int main() {
+  int n = 0;
+  while (true) {
+    cin >> n;
+    GWC(n, 2, 2, 3);
+    cout << endl;
+  }
+}
